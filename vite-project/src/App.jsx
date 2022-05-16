af5f48359ca0b2578a9532a5b16d3d8654db7928
dumeccs <88063009+dumeccs@users.noreply.github.com>
2022-05-16 23:40:49 +0100

    vite project initial commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Nav from './Components/Nav'
+function App() {
+  
+
+  return (
+    <div className="App">
+        <Nav />
+    </div>
+  )
+}
+
+export default App
---
+++ vite-project/src/Components/Nav.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav >
+            <div className='nav-left'>
+                <h2>Snap</h2>
+                <label htmlFor="Features">Features</label>
+                <select name="features" id="feature">
+                    <option value="Todo List">Todo List</option>
+                    <option value="Calendar" >Calendar</option>
+                    <option value="Reminder" >Reminder</option>
+                    <option value="Planning" >Planning</option>
+                </select>
+                <label htmlFor="Company">Company</label>
+                <select name="Company" id="Company">
+                    <option value="History">History</option>
+                    <option value="Our Team" >Our Team</option>
+                    <option value="Blog" >Blog</option>
+                </select>
+                <p>Careers</p>
+                <p>About</p>
+            </div>
+            <div className='nav-right'>
+                <p>Login</p>
+                <button>
+                    Register
+                </button>
+
+            </div>
+    </nav>
+  )
+}
+
+export default Nav
